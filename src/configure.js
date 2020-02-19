137c445c62d603e25c5096a86d93b5427452eeb7
Nightglowie <sophie.pages8@orange.fr>
2020-02-19 15:21:02 +0100

    changes

--- src/configure.js
+++ src/configure.js
@@ -2,6 +2,7 @@ const path = require('path')
 const mixme = require('mixme')
 config_default = require('../conf/default')
 
+
 module.exports = (config_user = {}) => {
   config = mixme.merge(config_default, config_user)
   if (config.users.db_dir)
